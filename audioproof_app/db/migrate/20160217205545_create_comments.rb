b7c794b60c36021fe508cadd5041704986dff33c
nickchurch <nickchurch10@gmail.com>
2016-02-17 13:09:29 -0800

    Added the database field 'anonymous_comment' to the Comment model, so users can mark a comment as anonymous
    Removed a .swp file because I committed while vim was open

--- audioproof_app/db/migrate/20160217205545_create_comments.rb
+++ audioproof_app/db/migrate/20160217205545_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :comment_body
+      t.boolean :anonymous_comment
       t.references :User, index: true, foreign_key: true
       t.references :Post, index: true, foreign_key: true
 
