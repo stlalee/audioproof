decfe9a946869aa7ba65f3d9812d3149804c63b0
Ryan Fulscher <rfulsche@ucsc.edu>
2016-02-29 17:15:50 -0800

    oops wrong branch name

--- audioproof_app/app/controllers/users_controller.rb
+++ audioproof_app/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+     @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page])
