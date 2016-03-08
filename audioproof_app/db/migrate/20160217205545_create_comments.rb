fba6c04f24c6c221662d451050dfd0d6b90c8517
nickchurch <nickchurch10@gmail.com>
2016-03-08 12:57:29 -0800

    Comments working - almost

--- audioproof_app/db/migrate/20160217205545_create_comments.rb
+++ audioproof_app/db/migrate/20160217205545_create_comments.rb
@@ -3,9 +3,8 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :comment_body
       t.boolean :anonymous_comment
-      t.references :User, index: true, foreign_key: true
-      t.references :Post, index: true, foreign_key: true
-
+      t.references :user, index: true, foreign_key: 'user_id'
+      t.references :post, index: true, foreign_key: 'post_id'
       t.timestamps null: false
     end
 	add_index :comments, [:post_id, :created_at]
