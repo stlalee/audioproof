55a9bd959b26c84f84ceac1e593fa75dd7b89ef3
Norberto Sanchez <nsanche5@ucsc.edu>
2016-03-15 01:15:24 -0700

    Making posts a bit neater, added image for posts with no image uploaded

--- audioproof_app/config/initializers/assets.rb
+++ audioproof_app/config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( synths.jpg )
+Rails.application.config.assets.precompile += %w( no-img.jpg )
