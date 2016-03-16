e6d9c97c1234587c395439a1a2f2e02c2f9e6b61
Stella Lee <stjilee@ucsc.edu>
2016-03-15 17:31:23 -0700

    added all sprint docs, release plan, bugs etc. and worked on home page layouts, will merge to master soon

--- audioproof_app/db/schema.rb
+++ audioproof_app/db/schema.rb
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 20160218004929) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "username"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
